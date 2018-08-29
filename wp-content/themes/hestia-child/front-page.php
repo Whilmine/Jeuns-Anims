438a2e572b6e922fbf7e4f3ef5ea57511a3334c9
Clair5D5l5p55 <claire.delepee@viacesi.fr>
2018-08-29 22:28:22 +0200

    modif sur la home, deuxieme partie

--- wp-content/themes/hestia-child/front-page.php
+++ wp-content/themes/hestia-child/front-page.php
@@ -36,48 +36,51 @@ if ( ! is_page_template() ) {
     <img class="absolute" id="trompette-pic" src=" <?php echo site_url(); ?>/wp-content/themes/hestia-child/assets/img/trompette.png">
 </div>
 	    <div class="<?php echo esc_attr( hestia_layout() ); ?>">
-            <h2 class="titlefont white first-title border-title"> A la une </h2>
-            <div class="grid">
-                <div  id="a-la-une" class="card">
-                    <?php $the_query = new WP_Query( 'posts_per_page=1' ); ?>
-                    <!-- Reste à écrire la condition : si un article existe dans la catégorie a la une, on l'affiche a la place -->
-                    <!-- dans l'autre cas , on affiche l'article le plus recent -->
-                    <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-                        <img src="<?php echo the_post_thumbnail_url();?>">
-                        <a class="titlefont first-title-color third-title" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-                        <p><?php the_excerpt() ?></p>
-                    <?php
-                    endwhile;
-                    wp_reset_postdata();
-                    ?>
-                </div>
-
-                <div id="otherevents">
-                    <span class="secondary-title titlefont accentblue border-title">Les autres événements</span>
-                    <ul>
-                        <?php $recentarticles = new WP_Query( 'posts_per_page=4' ); ?>
-                        <!-- Pour le moment je n'ai pas réussi a dégager le premier post, donc je triche en le dégageant via le css
-                        il faut compléter la fonction pour que si il y'a une article a la une on ne dégage pas le premier, et que les articles de catégories a la une soit dégagés -->
-                        <?php while ($recentarticles -> have_posts()) : $recentarticles -> the_post(); ?>
-                       <li>
-                           <!-- j'aimerais bien charger la petite tailles des images  ici  , mais j'y arrive pas momentanément-->
-                           <span class="featured-img" style="background-image: url('<?php echo the_post_thumbnail_url()?>')"></span>
-                           <span class="flex-column">
-                                <a class="titlefont white" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-                                <p><?php the_excerpt() ?></p>
-                               <a href="<?php echo the_permalink();?>"> <span class="btn-shape">Lire la suite</span></a>
-                           </span>
-
-                       </li>
-
+            <section id="a-la-une-part">
+                <h2 class="titlefont white first-title border-title"> A la une </h2>
+                <div class="grid">
+                    <div  id="a-la-une" class="card">
+                        <?php $the_query = new WP_Query( 'posts_per_page=1' ); ?>
+                        <!-- Reste à écrire la condition : si un article existe dans la catégorie a la une, on l'affiche a la place -->
+                        <!-- dans l'autre cas , on affiche l'article le plus recent -->
+                        <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+                            <img src="<?php echo the_post_thumbnail_url();?>">
+                            <a class="titlefont first-title-color third-title" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                            <p><?php the_excerpt() ?></p>
                         <?php
                         endwhile;
                         wp_reset_postdata();
                         ?>
-                    </ul>
-                    Voir tout
+                    </div>
+
+                    <div id="otherevents">
+                        <span class="secondary-title titlefont accentblue border-title">Les autres événements</span>
+                        <ul>
+                            <?php $recentarticles = new WP_Query( 'posts_per_page=4' ); ?>
+                            <!-- Pour le moment je n'ai pas réussi a dégager le premier post, donc je triche en le dégageant via le css
+                            il faut compléter la fonction pour que si il y'a une article a la une on ne dégage pas le premier, et que les articles de catégories a la une soit dégagés -->
+                            <?php while ($recentarticles -> have_posts()) : $recentarticles -> the_post(); ?>
+                           <li>
+                               <!-- j'aimerais bien charger la petite tailles des images  ici  , mais j'y arrive pas momentanément-->
+                               <span class="featured-img" style="background-image: url('<?php echo the_post_thumbnail_url()?>')"></span>
+                               <span class="flex-column">
+                                    <a class="titlefont white link-title" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                                    <p> <?php the_excerpt() ?></p>
+                                   <a href="<?php echo the_permalink();?>"> <span class="btn-shape">Lire la suite</span></a>
+                               </span>
+
+                           </li>
+
+                            <?php
+                            endwhile;
+                            wp_reset_postdata();
+                            ?>
+                        </ul>
+                        <div class="white border-title link-title">  Voir tout</div>
+                    </div>
                 </div>
-            </div>
+            </section>
+
 
             <?php // On revoie la de la page équipe
             $post_id = 54;
@@ -90,9 +93,11 @@ if ( ! is_page_template() ) {
                 <h2 class="titlefont white first-title border-title"><?php echo $name;?></h2>
                 <div class="card auto-width">
                     <div class="flex-row">
-                    <img src="<?php echo $img ?>">
-                        <div class="flex-column">
-                            <span class="first-title-color titlefont">Jeun's Anims</span>
+                        <div class="team-container image">
+                            <img src="<?php echo $img ?>">
+                        </div>
+                        <div class="flex-column team-container">
+                            <span class="first-title-color titlefont secondary-title">Jeun's Anims</span>
                             <p>
                                 <?php echo $post->post_content;?>
                             </p>
@@ -105,21 +110,22 @@ if ( ! is_page_template() ) {
 
                                          ) ); ?>
                             </nav>
-                            <!-- <span>
-                                <btn class="btn btn-primary">Lien 1</btn>
-                                <btn class="btn btn-primary">Lien 2</btn>
-                            </span> -->
                         </div>
                     </div>
                 </div>
             </div>
-                <section id="team-part">
+                <section id="contact-part">
 
             <h2 class="titlefont white first-title border-title"> Contactez-nous</h2>
-            <div class="card auto-width">
-                Contact
+            <div class="card auto-width flex-row">
+                <div class="team-container image">
+                    <img src="<?php echo $img ?>">
+                </div>
+                <div class="first-title-color titlefont secondary-title">
+                    Contact & Questions
+                </div>
             </div>
-
+                </section>
 
 
 
