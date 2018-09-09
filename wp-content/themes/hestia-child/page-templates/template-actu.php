60ec09597f21b39ce909b52d6616fc9ba2a88ffe
Clair5D5l5p55 <claire.delepee@viacesi.fr>
2018-09-09 02:44:30 +0200

    fix sur la home + page actus

--- wp-content/themes/hestia-child/page-templates/template-actu.php
+++ wp-content/themes/hestia-child/page-templates/template-actu.php
@@ -26,23 +26,26 @@ get_header( "pages" );
     $myposts = get_posts('');
     foreach($myposts as $post) :
         setup_postdata($post);
-        ?>
-    <div class="card" style="background-image: url('<?php the_post_thumbnail_url();?>')">
-        <a href="<?php echo the_permalink()?>">
-            <div class="card-content">
-                <span class="first-title-color fourth-title titlefont">
-                    <?php the_title(); ?>
-                </span>
 
-              <span class="second-title-color titlefont">
-                  <?php $date = get_post_custom_values("date");
-                    echo  $date[0];?>
-              </span>
+    if (has_post_thumbnail()) : ?> <div class="card" style="background-image:url('<?php echo the_post_thumbnail_url();?>')">
+            <?php else: ?>
+            <div class="card" style="background-image: url('/wp-content/themes/hestia-child/assets/img/background.jpg')"><?php endif; ?>
+                <a href="<?php echo the_permalink()?>">
+                    <div <?php if(has_post_thumbnail()) : ?> class="card-content"><?php endif;?>
+                        <span class="first-title-color fourth-title titlefont">
+                            <?php the_title(); ?>
+                        </span>
+                        <span class="second-title-color titlefont">
+                              <?php $date = get_post_custom_values("date");
+                                echo  $date[0];?>
+                         </span>
+                        <?php if (has_post_thumbnail()): echo ""; else:?>)
+                            <span>
+                                <?php the_excerpt()?>
+                            </span> <?php endif; ?>
+                    </div>
+                 </a>
             </div>
-        </a>
-
-
-    </div>
     <?php endforeach; wp_reset_postdata(); ?>
 
 
