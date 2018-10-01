a010c66d2f458a00381cfa0c2fd9e7fbdfa720ca
Clair5D5l5p55 <claire.delepee@viacesi.fr>
2018-10-01 14:29:34 +0200

    test

--- wp-content/themes/hestia-child/header-blog.php
+++ wp-content/themes/hestia-child/header-blog.php
@@ -8,13 +8,13 @@
  * @since Hestia 1.0
  */
 $wrapper_div_classes = 'wrapper ';
-if ( is_single() ) {
-    $wrapper_div_classes .= join( ' ', get_post_class() );
+if (is_single()) {
+    $wrapper_div_classes .= join(' ', get_post_class());
 }
 
 $header_class = '';
-$hide_top_bar = get_theme_mod( 'hestia_top_bar_hide', true );
-if ( (bool) $hide_top_bar === false ) {
+$hide_top_bar = get_theme_mod('hestia_top_bar_hide', true);
+if ((bool)$hide_top_bar === false) {
     $header_class .= 'header-with-topbar';
 }
 
@@ -22,33 +22,101 @@ if ( (bool) $hide_top_bar === false ) {
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset='<?php bloginfo( 'charset' ); ?>'>
+    <meta charset='<?php bloginfo('charset'); ?>'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php if (is_singular() && pings_open(get_queried_object())) : ?>
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php endif; ?>
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="particles-js" class="absolute" style="width: 100%; z-index: -1"></div>
-<div class="<?php echo esc_attr( $wrapper_div_classes ); ?> main-template blog-template">
+<div class="<?php echo esc_attr($wrapper_div_classes); ?> main-template blog-template">
 
 
     <header>
-        <div class=" container header white <?php echo esc_attr( $header_class ); ?>" >
+        <div class=" container header white <?php echo esc_attr($header_class); ?>">
             <span class="main-title first-title titlefont">Article</span>
             <div class="title-logo-wrapper">
-                <a class="navbar-brand" ></a>
+                <a class="navbar-brand"></a>
             </div>
         </div>
         <div style="height: 2px; background-color: #FFffff; width: 80%"></div>
     </header>
 
-    <?php do_action( 'hestia_do_header' ); ?>
-
+    <?php do_action('hestia_do_header'); ?>
 
 
     <script type="text/javascript">
-        particlesJS("particles-js", {"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5287372649289357,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3.9458004845442964,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"right","random":true,"straight":true,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"window","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":76.01351351351346,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;    </script>
+        particlesJS("particles-js", {
+            "particles": {
+                "number": {
+                    "value": 160,
+                    "density": {"enable": true, "value_area": 800}
+                },
+                "color": {"value": "#ffffff"},
+                "shape": {
+                    "type": "circle",
+                    "stroke": {"width": 0, "color": "#000000"},
+                    "polygon": {"nb_sides": 5},
+                    "image": {"src": "img/github.svg", "width": 100, "height": 100}
+                },
+                "opacity": {
+                    "value": 0.5287372649289357,
+                    "random": true,
+                    "anim": {"enable": true, "speed": 1, "opacity_min": 0, "sync": false}
+                },
+                "size": {
+                    "value": 3.9458004845442964,
+                    "random": true,
+                    "anim": {"enable": false, "speed": 4, "size_min": 0.3, "sync": false}
+                },
+                "line_linked": {"enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1},
+                "move": {
+                    "enable": true,
+                    "speed": 1,
+                    "direction": "right",
+                    "random": true,
+                    "straight": true,
+                    "out_mode": "out",
+                    "bounce": false,
+                    "attract": {"enable": false, "rotateX": 600, "rotateY": 600}
+                }
+            },
+            "interactivity": {
+                "detect_on": "window",
+                "events": {
+                    "onhover": {"enable": true, "mode": "repulse"},
+                    "onclick": {"enable": true, "mode": "repulse"},
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {"distance": 400, "line_linked": {"opacity": 1}},
+                    "bubble": {"distance": 250, "size": 0, "duration": 2, "opacity": 0, "speed": 3},
+                    "repulse": {"distance": 76.01351351351346, "duration": 0.4},
+                    "push": {"particles_nb": 4},
+                    "remove": {"particles_nb": 2}
+                }
+            },
+            "retina_detect": true
+        });
+        var count_particles, stats, update;
+        stats = new Stats;
+        stats.setMode(0);
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        document.body.appendChild(stats.domElement);
+        count_particles = document.querySelector('.js-count-particles');
+        update = function () {
+            stats.begin();
+            stats.end();
+            if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+                count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+            }
+            requestAnimationFrame(update);
+        };
+        requestAnimationFrame(update);
+        ;    </script>
--- wp-content/themes/hestia-child/page-templates/template-trombinoscope.php
+++ wp-content/themes/hestia-child/page-templates/template-trombinoscope.php
@@ -9,85 +9,80 @@
  */
 
 //get_header();
-get_header( "pages" );
- ?>
-
-
-
+get_header("pages");
+?>
 
 
 <div id="trombinoscope" class="<?php echo hestia_layout(); ?>">
     <span class="accentblue titlefont secondary-title title">Les membres du bureau</span>
     <div class="flex-row flex-wrap">
-    <?php
-   $loop = new WP_Query( array( 'post_type' => 'members', 'posts_per_page' => '10' ) );
-    $instrumentArray = array( array("note-pic","/wp-content/themes/hestia-child/assets/img/note.png"), array("maracas-pic","/wp-content/themes/hestia-child/assets/img/maracas.png"),
-     array("guitar-pic","/wp-content/themes/hestia-child/assets/img/guitar.png"), array("saxo-pic","/wp-content/themes/hestia-child/assets/img/saxo.png"), array("tambour-pic","/wp-content/themes/hestia-child/assets/img/tambour.png"),array("saxo-pic","/wp-content/themes/hestia-child/assets/img/saxo.png"), array("fa-pic","/wp-content/themes/hestia-child/assets/img/fa.png"));
-    $i=0;
+        <?php
+        $loop = new WP_Query(array('post_type' => 'members', 'posts_per_page' => '0'));
+        $instrumentArray = array(array("note-pic", "/wp-content/themes/hestia-child/assets/img/note.png"), array("maracas-pic", "/wp-content/themes/hestia-child/assets/img/maracas.png"),
+            array("guitar-pic", "/wp-content/themes/hestia-child/assets/img/guitar.png"), array("saxo-pic", "/wp-content/themes/hestia-child/assets/img/saxo.png"), array("tambour-pic", "/wp-content/themes/hestia-child/assets/img/tambour.png"), array("saxo-pic", "/wp-content/themes/hestia-child/assets/img/saxo.png"), array("fa-pic", "/wp-content/themes/hestia-child/assets/img/fa.png"));
+        $i = 0;
 
-    while ( $loop->have_posts() ) : $loop->the_post();
-        $meta = get_post_meta( $post->ID, 'your_fields', true );
-    if ($meta['checkbox'] === 'checkbox'){
-    $thumbnail_id = get_post_thumbnail_id($post->ID);
-    $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+        while ($loop->have_posts()) :
+        $loop->the_post();
+        $meta = get_post_meta($post->ID, 'your_fields', true);
+        if ($meta['checkbox'] === 'checkbox'){
+        $thumbnail_id = get_post_thumbnail_id($post->ID);
+        $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 
-    ?>
-         <div class="card-wrapper">
-             <div class='card'>
-                 <img class="absolute" id="<?php echo $instrumentArray[$i][0] ?>" alt="élement décoratif" src="<?php echo site_url().$instrumentArray[$i][1]?>">
-                 <?php $i++;
-                    if ($i >= 6){$i=0;
-                 }?>
-                 <img src="<?echo (get_the_post_thumbnail_url());?>" alt="<? echo $alt?>">
-         <?php
-          echo "<h3 class='titlefont first-title-color'>";
-          echo the_title();
-          echo "</h3>";
-          echo the_content();
-          echo "</div></div>";
-    };
-?>
-    <?php endwhile; wp_reset_query();
-    ?>
-    </div>
-
-
-
-
-    <?php
-    $testyloop = new WP_Query( array( 'post_type' => 'members', 'posts_per_page' => '10') );
-    if ( $testyloop->have_posts() ) : while ( $testyloop->have_posts() ) : $testyloop->the_post();
-    $metatety = get_post_meta( $post->ID, 'your_fields', true );
-    if ($metatety['checkbox'] != 'checkbox') :
-        $othermember = 'yes';
-         echo  '<span class="accentblue titlefont secondary-title">Les bénévoles</span>';
-    endif;
-    endwhile; endif;
+        ?>
+        <div class="card-wrapper">
+            <div class='card'>
+                <img class="absolute" id="<?php echo $instrumentArray[$i][0] ?>" alt="élement décoratif"
+                     src="<?php echo site_url() . $instrumentArray[$i][1] ?>">
+                <?php $i++;
+                if ($i >= 6) {
+                    $i = 0;
+                } ?>
+                <img src="<? echo(get_the_post_thumbnail_url()); ?>" alt="<? echo $alt ?>">
+                <?php
+                echo "<h3 class='titlefont first-title-color'>";
+                echo the_title();
+                echo "</h3>";
+                echo the_content();
+                echo "</div></div>";
+                };
+                ?>
+                <?php endwhile;
+                wp_reset_query();
+                ?>
+            </div>
 
 
+            <?php
+            $testyloop = new WP_Query(array('post_type' => 'members', 'posts_per_page' => '10'));
+            if ($testyloop->have_posts()) : while ($testyloop->have_posts()) : $testyloop->the_post();
+                $metatety = get_post_meta($post->ID, 'your_fields', true);
+                if ($metatety['checkbox'] != 'checkbox') :
+                    $othermember = 'yes';
+                    echo '<span class="accentblue titlefont secondary-title">Les bénévoles</span>';
+                endif;
+            endwhile; endif;
 
 
-    if ($othermember == 'yes'){
-        $secondloop = new WP_Query( array( 'post_type' => 'members', 'posts_per_page' => '10') );
-            if ( $secondloop->have_posts() ) : while ( $secondloop->have_posts() ) : $secondloop->the_post();
-             $meta = get_post_meta( $post->ID, 'your_fields', true );
-                if ($meta['checkbox'] != 'checkbox') {
-                    echo "<div class='card-wrapper'><div class='card'><img src='";
-                    echo (get_the_post_thumbnail_url())."'>";
-                    echo  "<h3 class='titlefont first-title-color'>";
-                    echo the_title();
-                    echo "</h3>";
-                    echo the_content();
-                    echo "</div></div>";
-                }
-             endwhile;
-             endif;
-              wp_reset_query();
-    }
+            if ($othermember == 'yes') {
+                $secondloop = new WP_Query(array('post_type' => 'members', 'posts_per_page' => '10'));
+                if ($secondloop->have_posts()) : while ($secondloop->have_posts()) : $secondloop->the_post();
+                    $meta = get_post_meta($post->ID, 'your_fields', true);
+                    if ($meta['checkbox'] != 'checkbox') {
+                        echo "<div class='card-wrapper'><div class='card'><img src='";
+                        echo (get_the_post_thumbnail_url()) . "'>";
+                        echo "<h3 class='titlefont first-title-color'>";
+                        echo the_title();
+                        echo "</h3>";
+                        echo the_content();
+                        echo "</div></div>";
+                    }
+                endwhile;
+                endif;
+                wp_reset_query();
+            }
+            ?>
+        </div>
 
 
-    ?>
-    </div>
-
-
-	<?php get_footer(); ?>
+        <?php get_footer(); ?>
